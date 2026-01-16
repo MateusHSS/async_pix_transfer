9dec3229e292a5648dbae81747df38aec95437b9
Mateus Silva <dev.mateushssilva@gmail.com>
2026-01-16 10:54:37 -0300

    Add evento e listener de transferencia

---
+++ pix-worker/src/main/java/com/example/domain/TransferenciaEvent.java
@@ -0,0 +1,8 @@
+package com.example.domain;
+
+import io.micronaut.serde.annotation.Serdeable;
+
+import java.math.BigDecimal;
+
+@Serdeable
+public record TransferenciaEvent(String clienteId, BigDecimal valor, String chaveDestino) {}
