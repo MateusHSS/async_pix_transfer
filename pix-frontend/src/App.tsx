92e4e538cf40fb3e6ca1d5a3d46ff1f2e06d3203
Mateus Silva <dev.mateushssilva@gmail.com>
2026-02-11 18:46:12 -0300

    Implementa menu de navegação

--- pix-frontend/src/App.tsx
+++ pix-frontend/src/App.tsx
@@ -1,13 +1,24 @@
 import { Container, Box } from '@mui/material';
+import { useState } from 'react';
+import { Navigation } from './components/Navigation';
 import { PixForm } from './features/transferencias/components/PixForm';
+import { CadastroClienteForm } from "./features/cliente/components/CadastroClienteForm.tsx";
+
+type MenuOption = 'cadastro' | 'transferencia';
 
 function App() {
+    const [activeMenu, setActiveMenu] = useState<MenuOption>('cadastro');
+
     return (
-        <Container maxWidth="md">
-            <Box sx={{ py: 4 }}>
-                <PixForm />
-            </Box>
-        </Container>
+        <>
+            <Navigation onMenuSelect={setActiveMenu} activeMenu={activeMenu} />
+            <Container maxWidth="md">
+                <Box sx={{ py: 4 }}>
+                    {activeMenu === 'cadastro' && <CadastroClienteForm />}
+                    {activeMenu === 'transferencia' && <PixForm />}
+                </Box>
+            </Container>
+        </>
     );
 }
 
